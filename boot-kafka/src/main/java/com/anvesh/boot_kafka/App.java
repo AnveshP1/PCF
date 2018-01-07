b75ea15bb8ff1a82a8884306ebd6eca364e67e7c
AnveshP1 <33906487+AnveshP1@users.noreply.github.com>
2018-01-07 12:43:14 -0600

    Add files via upload

---
+++ boot-kafka/src/main/java/com/anvesh/boot_kafka/App.java
@@ -0,0 +1,13 @@
+package com.anvesh.boot_kafka;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
